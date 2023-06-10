e9a342dc8891feb170c773c7484dd800ef0f64e5
antoims <antoine.gros14@gmail.com>
2023-06-10 15:06:38 +0200

    quasi fini

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,10 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application";
+import { application } from "./application"
 
-import FormController from "./form_controller";
-application.register("form", FormController);
+import FormController from "./form_controller"
+application.register("form", FormController)
+
+import NoticeController from "./notice_controller"
+application.register("notice", NoticeController)
